c3107dd6b957f9576d7404582acf141961d75d01
flytang <172340209@qq.com>
2022-07-29 15:01:04 +0800

    list usb ports info

--- Examples/Hello_UsbTree/Program.cs
+++ Examples/Hello_UsbTree/Program.cs
@@ -7,16 +7,29 @@ namespace Hello_UsbTree
     {
         static void Main(string[] args)
         {
+            Guid hub = new Guid("36fc9e60-c465-11cf-8056-444553540000");
             using (var usbTree = new UsbTree())
             {
                 foreach (var device in usbTree.DeviceList)
                 {
-                    Console.WriteLine(device);
+                    if (((lunOptics.libUsbTree.UsbDevice)device).ClassDescription.Equals("USB") &&
+                        !((lunOptics.libUsbTree.UsbDevice)device).ClassGuid.Equals(hub) && true)
+                    {
+                        Console.WriteLine(device);
+                        foreach (var fun in device.functions)
+                        {
+                            String classDescription = ((lunOptics.libUsbTree.UsbDevice)fun).ClassDescription;
+                            if (classDescription.Equals("Ports"))
+                                Console.WriteLine("\t" + classDescription + " " + fun.Description + " [MI_" + fun.Mi +
+                                                  "]");
+                        }
+                    }
                 }
             }
 
             Console.WriteLine("\n\nAny key to exit");
-            while (!Console.KeyAvailable) ;
+            while (!Console.KeyAvailable)
+                ;
         }
     }
 }
